db73a306ba2759ce7ce8283d96ce07c6e4f2e359
Moin-A <moinqa6@gmail.com>
2021-08-11 16:13:24 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/output.css";
 import Page from "../components/Page";
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Nav from "../components/Nav";
 import Product from "../components/Products";
 import { createContext } from "react";
 const Provider = createContext();
